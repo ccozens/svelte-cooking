b74db137636557e60a10ed4af1368376a1ff1b0e
ccozens <ccozens@gmail.com>
2024-02-14 13:59:59 +0000

    added colours as CSS variables to app.pcss html and added colour and hover styles to ListItem.svelte

--- src/lib/components/ListItem.svelte
+++ src/lib/components/ListItem.svelte
@@ -23,14 +23,24 @@
 	}
 
 	.card {
-		flex-basis: var(--size-content-1);
 		display: flex;
 		flex-direction: column;
 		gap: var(--size-2);
 		background: var(--surface-3);
-		border: 1px solid var(--surface-1);
+		border: 1px solid var(--highlight);
 		padding: var(--size-4);
-		border-radius: var(--radius-3);
-		box-shadow: var(--shadow-2);
+		border-radius: var(--size-1);
+		box-shadow: var(--size-2);
+
+        & a {
+            text-decoration: none;
+            color: var(--text-color);
+            font-size: 1.5rem;
+            text-transform: capitalize;
+        }
+
+        &:hover {
+            background: var(--surface-hover);
+        }
 	}
 </style>
