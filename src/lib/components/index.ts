b16fe567d04f3de9e977e92d153aa14517b60bbc
ccozens <ccozens@gmail.com>
2024-02-17 11:11:39 +0000

    added index.ts files to src/components and subfolders

---
+++ src/lib/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Header} from './Header.svelte';
+
+export { default as ListItem } from './ListItem.svelte';
+
+export { default as RecipeList } from './RecipeList.svelte';
---
+++ src/lib/components/recipe/index.ts
@@ -0,0 +1,7 @@
+export { default as Ingredients } from './Ingredients.svelte';
+
+export { default as Step } from './Step.svelte';
+
+export { default as StepList } from './StepList.svelte';
+
+export { default as Title } from './Title.svelte';
