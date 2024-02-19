0739896eff7597e3fdaa4bf7fbef903d5c9f0e47
ccozens <ccozens@gmail.com>
2024-02-19 10:10:09 +0000

    added click handler and conditional style to Button.svelte so fades on click

--- src/lib/components/common/Button.svelte
+++ src/lib/components/common/Button.svelte
@@ -1,4 +1,13 @@
-<button>
+<script lang="ts">
+	// create done flag
+	export let done = false;
+
+	function handleClick() {
+		done = !done;
+	}
+</script>
+
+<button on:click={handleClick} class:done>
 	<slot>Button</slot>
 </button>
 
@@ -6,14 +15,18 @@
 	button {
 		font-size: var(--font-size-fluid-2);
 		line-height: 1.5;
-        text-align: left;
-        padding-inline: var(--font-size-fluid-1);
-        margin: var(--size-1);
-        background-color: var(--teal-12);
-        width: 90%;
+		text-align: left;
+		padding-inline: var(--font-size-fluid-1);
+		margin: var(--size-1);
+		background-color: var(--teal-12);
+		width: 90%;
 
 		@media screen and (min-width: 768px) {
 			font-size: var(--font-size-fluid-1);
 		}
+	}
+
+	.done {
+		opacity: 0.1;
 	}
 </style>
