86464bb16db74de8449271db1eea55b6fef276ec
ccozens <ccozens@gmail.com>
2024-02-21 13:58:00 +0000

    added if clause to Title.svelte to display source as an a element if its a link

--- src/lib/components/recipe/Title.svelte
+++ src/lib/components/recipe/Title.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
-
-    export let name: string;
-    export let source: string;
+	export let name: string;
+	export let source: string;
 </script>
 
 <h2>{name}</h2>
-<cite>From {source}</cite>
+<cite
+	>From
+	{#if source.includes('http')}
+		<a href={source} target="_blank">{source}</a>
+	{:else}
+		{source}
+	{/if}
+</cite>
 
 <style lang="postcss">
-    h2 {
-        text-transform: capitalize;
-    }
+	h2 {
+		text-transform: capitalize;
+	}
 </style>
