3f9bba0808708fe6c06a3041cd65dcbfeb0c1b7f
ccozens <ccozens@gmail.com>
2024-05-15 16:25:07 +0100

    added plus icon to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,3 +11,18 @@
 
 <Header />
 <RecipeList items={id_and_names_and_slugs} />
+
+<i class="fa-solid fa-circle-plus"></i>
+
+<style lang="postcss">
+	i {
+		position: fixed;
+		bottom: var(--size-4);
+		right: var(--size-4);
+		font-size: var(--font-size-5);
+		border: var(--border-size-2) solid var(--lime-5);
+		border-radius: 100%;
+		color: var(--surface-2);
+		background-color: var(--lime-5);
+	}
+</style>
