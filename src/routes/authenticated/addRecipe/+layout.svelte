b27da69e6efbac0f20ae12d278074364089930b2
ccozens <ccozens@gmail.com>
2024-05-30 12:59:19 +0100

    restored addRecipe page.svelte from commit e338dcd

--- src/routes/authenticated/addRecipe/+layout.svelte
+++ src/routes/authenticated/addRecipe/+layout.svelte
@@ -1,1 +1,102 @@
-<h1>heree?</h1>
+<script lang="ts">
+    let { children, data } = $props<{ children: Snippet }>();
+
+    let { session }      = data;
+
+    let { name }         = session.user;
+    $: console.log(session.user);
+    $: console.log(name);
+    let isLoggedIn = $state(true);
+
+
+</script>
+
+{@render children()}
+
+<header class="header">
+    <div class="signedInStatus">
+        <p class="loaded">
+            {#if isLoggedIn}
+
+            <span class="signedInText">
+                <small>Signed in as</small>
+                <strong>{name}</strong>
+            </span>
+            <a href="/addRecipe/signout" class="button" data-sveltekit-preload-data="off">Sign out</a>
+            {:else}
+            <span class="notSignedInText">You are not signed in</span>
+            <a href="/addRecipe/signin" class="buttonPrimary" data-sveltekit-preload-data="off">Sign in</a>
+            {/if}
+        </p>
+    </div>
+</header>
+
+<style lang="postcss">
+    /* Header styles */
+    header.header {
+        display: flex; /* Allow side-by-side elements */
+        justify-content: space-between; /* Space items horizontally */
+        align-items: center; /* Align vertically */
+        padding: 1rem; /* Add some padding */
+        box-sizing: border-box; /* Ensure padding is included in width */
+    }
+
+    .signedInStatus {
+        display: flex;
+        align-items: center;
+    }
+
+    .avatar {
+        width: 32px;
+        height: 32px;
+        border-radius: 50%;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+    }
+
+    .signedInText {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        margin-left: 0.5rem;
+    }
+
+    .notSignedInText {
+        display: none;
+    }
+
+
+    .loaded .notSignedInText {
+        display: block;
+    }
+
+    .loaded .signedInText {
+        display: none;
+    }
+
+    .button {
+        cursor: pointer;
+    }
+
+    .buttonPrimary {
+        color: white;
+        background-color: var(--lime-5);
+    }
+
+    .buttonPrimary:hover {
+        color: var(--lime-5);
+        background-color: white;
+    }
+
+    .buttonPrimary:focus {
+        outline: none;
+    }
+
+    .buttonPrimary:active {
+        background-color: var(--lime-5);
+        color: white;
+    }
+
+
+</style>
--- src/routes/authenticated/addRecipe/+page.svelte
+++ src/routes/authenticated/addRecipe/+page.svelte
@@ -1,7 +1,66 @@
 <script lang="ts">
-  </script>
+	import { enhance } from '$app/forms';
+	import FormField from '$lib/components/common/FormField.svelte';
+</script>
 
-  <h1>SvelteKit Auth Example</h1>
+<form method="POST" use:enhance>
+	<FormField label="Name" type="text" id="name" name="name" placeholder="Recipe name"
+	autocomplete="off" required />
+	<FormField
+		label="Source"
+		type="text"
+		id="source"
+		name="source"
+		placeholder="Recipe URL, book page, etc"
+		autocomplete="off"
+		required
+	/>
+	<FormField
+		label="Ingredients"
+		type="textarea"
+		id="ingredients"
+		name="ingredients"
+		placeholder="Paste in ingredients - the app will format them"
+		autocomplete="off"
+		required
+	/>
+	<FormField
+		label="Steps"
+		type="textarea"
+		id="steps"
+		name="steps"
+		placeholder="Paste in steps - the app will format them"
+		autocomplete="off"
+		required
+	/>
+	<FormField
+		label="Password"
+		type="password"
+		id="password"
+		name="password"
+		placeholder="Enter password"
+		autocomplete="off"
+		required
+	/>
+	<button type="submit">Submit</button>
+</form>
 
+<style lang="postcss">
+	form {
+		display: grid;
+		gap: 1rem;
+		padding: 1rem;
+		margin: 1rem;
 
-  <h1>Add Recipe</h1>
+		& button {
+			background: var(--gradient-30);
+			border: var(--border-size-2) solid var(--teal-12);
+			border-radius: var(--radius);
+			box-shadow: var(--size-2);
+			padding: var(--size-2);
+			font-size: var(--font-size-3);
+			text-transform: uppercase;
+			cursor: pointer;
+		}
+	}
+</style>
